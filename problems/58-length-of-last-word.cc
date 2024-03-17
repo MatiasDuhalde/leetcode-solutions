008504beeec845de6e3e78d2c822170f9eaea21d
Matías Duhalde <contact@duhal.de>
2024-03-17 16:15:33 +0100

    feat: problem 58

---
+++ problems/58-length-of-last-word.cc
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=58 lang=cpp
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+#include <string>
+
+class Solution {
+public:
+  int lengthOfLastWord(std::string s) {}
+};
+// @lc code=end
